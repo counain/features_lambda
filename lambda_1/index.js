96992c6143741fbfd1beeb3977c22c6d2d9738fd
EC2 Default User <ec2-user@ip-10-0-4-25.ap-south-1.compute.internal>
2023-09-04 11:47:25 +0000

    lambda_1 updated

--- lambda_1/index.js
+++ lambda_1/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('lambda management  with multiple folders is working!'),
+    body: JSON.stringify('lambda management  with multiple folders is working using aws-assume-role!'),
   };
   return response;
 };
