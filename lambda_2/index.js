5c3162d15b60b2698c60b8a169e010466733e90d
EC2 Default User <ec2-user@ip-10-0-3-68.ap-south-1.compute.internal>
2023-09-08 10:20:20 +0000

    Determine most recently modified folder_2

--- lambda_2/index.js
+++ lambda_2/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('hi all i am  working on updating  Lambda_2 using lambda management method!'),
+    body: JSON.stringify('github last commit!'),
   };
   return response;
 };
