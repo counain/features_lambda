f70ef6d6c32327fda5f940b7dc12f80660326801
EC2 Default User <ec2-user@ip-10-0-3-68.ap-south-1.compute.internal>
2023-09-12 05:53:07 +0000

    automating lambda_2

--- lambda_1/index.js
+++ lambda_1/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('lambda automation by using github actions !'),
+    body: JSON.stringify('lambda automation_1!'),
   };
   return response;
 };
